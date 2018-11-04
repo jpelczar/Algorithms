465577356539b6dc0b1810e4fa5e8c4b73aed153
Jakub Pelczar <jakubpelczar92@gmail.com>
2018-11-04 14:10:42 +0100

    Added Ex2 merge

--- src/pl/sda/sort/MergeEx.java
+++ src/pl/sda/sort/MergeEx.java
@@ -6,11 +6,19 @@ public class MergeEx {
 
     public static void main(String[] args) {
         int[] array = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8};
+        int[] array2 = new int[]{9, 10, 11, 12, 13, 14, 15, 16, 17};
 
         System.out.println("Ex1 V1:");
         divArray(array);
         System.out.println("Ex1 V2:");
         divArrayV2(array);
+
+        System.out.println("Ex2 V1:");
+        System.out.println(Arrays.toString(concatSameSizeArrayV1(array, array2)));
+        System.out.println("Ex2 V2:");
+        System.out.println(Arrays.toString(concatSameSizeArrayV2(array, array2)));
+        System.out.println("Ex2 V3:");
+        System.out.println(Arrays.toString(concatSameSizeArrayV3(array, array2)));
     }
 
 
@@ -49,6 +57,54 @@ public class MergeEx {
 
         System.out.println(Arrays.toString(left));
         System.out.println(Arrays.toString(right));
+    }
+
+    public static int[] concatSameSizeArrayV1(int[] left, int[] right) {
+        if (left.length != right.length) {
+            return new int[0];
+        }
+
+        int[] result = new int[left.length + right.length];
+
+        int dodatkowaZmienna = 0;
+        for (int i = 0; i < result.length; i += 2) {
+            result[i] = left[dodatkowaZmienna];
+            result[i + 1] = right[dodatkowaZmienna];
+            dodatkowaZmienna++;
+        }
+
+        return result;
+    }
+
+    public static int[] concatSameSizeArrayV2(int[] left, int[] right) {
+        if (left.length != right.length) {
+            return new int[0];
+        }
+
+        int[] result = new int[left.length + right.length];
+
+        for (int i = 0; i < result.length; i += 2) {
+            result[i] = left[i / 2];
+            result[i + 1] = right[i / 2];
+        }
+
+        return result;
+    }
+
+
+    public static int[] concatSameSizeArrayV3(int[] left, int[] right) {
+        if (left.length != right.length) {
+            return new int[0];
+        }
+
+        int[] result = new int[left.length + right.length];
+
+        for (int i = 0; i < left.length; i++) {
+            result[2 * i] = left[i];
+            result[2 * i + 1] = right[i];
+        }
+
+        return result;
     }
 
 }
